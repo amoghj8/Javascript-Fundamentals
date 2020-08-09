9b9621d0108e038cef89f69e1671d3370c6c3e2b
Amogh Joshi <amoghj8@gmail.com>
2020-08-09 16:13:28 +0530

    Added static method

--- ES5 and ES2015/subclasses.js
+++ ES5 and ES2015/subclasses.js
@@ -15,7 +15,13 @@ class User extends Person {
         super(firstName, lastName);
         this.phone = phone;
     }
+
+    static getMembership() {
+        return "Standard Membership"
+    }
 }
 
 const user = new User('amogh', 'joshi', '123');
 console.log(user.greeting());
+console.log(User.getMembership());
+console.log(user);
